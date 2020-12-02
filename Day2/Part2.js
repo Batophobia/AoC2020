bef20bf463b10d733150bd683cc0a473adeb9483
Batophobia <godrox3@yahoo.com>
2020-12-02 12:37:39 -0600

    Update Part2.js

--- Day2/Part2.js
+++ Day2/Part2.js
@@ -1,1 +1,15 @@
-// TODO
+function aoc(){
+  ttl = 0;
+  for(r in a){
+    idx1 = a[r][0]-1;
+    idx2 = a[r][1]-1;
+    letter = a[r][2];
+    pass = a[r][3];
+    if(pass[idx1] == letter && pass[idx2] != letter){
+      ttl++;
+    } else if(pass[idx1] != letter && pass[idx2] == letter){
+      ttl++;
+    }
+  }
+  return ttl;
+}
