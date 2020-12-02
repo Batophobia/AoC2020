370339862b5b70fc319decf94528624047af9f66
Batophobia <godrox3@yahoo.com>
2020-12-02 12:28:02 -0600

    Update Part1.js

--- Day2/Part1.js
+++ Day2/Part1.js
@@ -1,1 +1,15 @@
-// TODO
+function aoc(){
+  ttl = 0;
+  for(r in a){
+    min = a[r][0];
+    max = a[r][1];
+    letter = a[r][2];
+    pass = a[r][3];
+    re = new RegExp(letter,"g");
+    arr = pass.match(re) || [];
+    if(arr.length >= min && arr.length <= max){
+      ttl++;
+    }
+  }
+  return ttl;
+}
