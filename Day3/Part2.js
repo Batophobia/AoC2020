599236a4f70c85810b21c266e8e991a1d92e8fea
Batophobia <godrox3@yahoo.com>
2020-12-03 10:25:08 -0600

    Update Part2.js

--- Day3/Part2.js
+++ Day3/Part2.js
@@ -1,3 +1,11 @@
-function aoc(){
-  // TODO
+function aoc(r,d){
+  wid = a[0].length;
+  y=0;x=0;ttl=0;
+  for(;y<a.length;y+=d){
+    if(a[y][x]) ttl++;
+    x+=r; x = x % wid;
+  }
+  return ttl;
 }
+
+//aoc(1,1)*aoc(3,1)*aoc(5,1)*aoc(7,1)*aoc(1,2)
