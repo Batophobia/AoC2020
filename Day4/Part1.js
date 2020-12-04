37d0b2d865d8151c381c87ac45e7c91fe4186262
Batophobia <godrox3@yahoo.com>
2020-12-04 10:12:52 -0600

    Update Part1.js

--- Day4/Part1.js
+++ Day4/Part1.js
@@ -1,3 +1,17 @@
 function aoc(){
-  // TODO
+  ipt = document.body.textContent || document.body.innerText;
+  ipt = ipt.split(/\n\n/g);
+  req = ["byr","iyr","eyr","hgt","hcl","ecl","pid"];
+  ttl = 0;
+  for(pas in ipt){
+    valid = true;
+    for(r in req){
+      if(ipt[pas].indexOf(req[r]+':') < 0){
+        valid=false;
+        break;
+      }
+    }
+    if(valid) ttl++;
+  }
+  return ttl;
 }
