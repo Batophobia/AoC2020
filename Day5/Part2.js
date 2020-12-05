f1490f2193a6478adcea869fe93e95cef0a0affc
Batophobia <godrox3@yahoo.com>
2020-12-05 01:04:42 -0600

    Update Part2.js

--- Day5/Part2.js
+++ Day5/Part2.js
@@ -1,3 +1,9 @@
 function aoc(){
-  // TODO
+  ipt = document.body.textContent || document.body.innerText;
+  ipt = ipt.replace(/[BR]/g,1).replace(/[FL]/g,0)
+  ipt = ipt.split(/\s+/g);
+  ipt = ipt.map(n => parseInt(n,2)).filter(r=>!!r).sort((a,b) => a-b);
+  for(i=0;++i<ipt.length;){
+    if(ipt[i] != ipt[i-1]+1) return [ipt[i-1], ipt[i]];
+  }
 }
