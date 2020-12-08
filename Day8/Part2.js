735374130e1907a9e49419ec22f02609946b86e0
Batophobia <godrox3@yahoo.com>
2020-12-08 11:27:56 -0600

    Update Part2.js
    Doesn't work yet

--- Day8/Part2.js
+++ Day8/Part2.js
@@ -1,4 +1,33 @@
+tries = {};
+function tryRun(ipt){
+  acc = 0;
+  jmp = 1;
+  prev = 0;
+  mp = {};
+  for(i=0; i<ipt.length; i+=jmp){
+    if(mp[i]) { console.log(acc); return prev; }
+    mp[i] = true;
+    jmp = 1;
+    c = ipt[i].split(" ");
+    switch(c[0]){
+      case "acc": acc += parseInt(c[1]); break;
+      case "jmp": jmp = parseInt(c[1]);
+      default: if(!tries[i]) prev=i; break;
+    }
+  }
+  return false;
+}
 function aoc(){
-  ipt = document.body.textContent || document.body.innerText;
-  // TODO
+  inpt = document.body.textContent || document.body.innerText;
+  inpt = inpt.split(/\n/g).filter(v => v);
+  newTry = [...inpt];
+  change = true;
+  while(change){
+  	change = tryRun(newTry);
+    if(change){
+      tries[change]=true;
+      newTry = [...inpt];
+      newTry[change].replace(/(jmp)|(nop)/, newTry[change].substr(0,3) == "nop" ? "jmp" : "nop");
+    }
+	}
 }
