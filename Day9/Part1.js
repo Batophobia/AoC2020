5973f9a4b2fc6f2a545268032e6cbbaf38f568ea
Batophobia <godrox3@yahoo.com>
2020-12-08 23:45:03 -0600

    Update Part1.js

--- Day9/Part1.js
+++ Day9/Part1.js
@@ -1,4 +1,13 @@
 function aoc(){
-  ipt = document.body.textContent || document.body.innerText;
-  // TODO
+  inpt = document.body.textContent || document.body.innerText;
+  inpt = inpt.split(/\n/g).filter(v => v).map(v=>parseInt(v));;
+  for(cur=25;i<inpt.length;cur++){
+    valid = false;
+    for(i=cur-25;!valid && i<cur-2;i++){
+      for(j=cur-1;!valid && j>i;j--){
+        if(inpt[j] + inpt[i] == inpt[cur]) valid = true;
+      }
+    }
+    if(!valid) return inpt[cur];
+  }
 }
