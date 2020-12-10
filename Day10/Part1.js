32ae97aca2ff50cdcb76aa022f3f096ce579033b
Batophobia <godrox3@yahoo.com>
2020-12-10 09:46:53 -0600

    Update Part1.js

--- Day10/Part1.js
+++ Day10/Part1.js
@@ -1,4 +1,13 @@
 function aoc(){
-  ipt = document.body.textContent || document.body.innerText;
-  // TODO
+  inpt = document.body.textContent || document.body.innerText;
+  inpt = inpt.split(/\n/g).filter(v => v).map(v=>parseInt(v));;
+  inpt = [0,...inpt.sort((a,b) => a-b)];
+  ttl = {1:0,3:1};
+  for(i=0;++i<inpt.length;){
+    switch(inpt[i] - inpt[i-1]){
+      case 1: ttl[1]++; break;
+      case 3: ttl[3]++; break;
+    }
+  }
+  return ttl[1] * ttl[3];
 }
